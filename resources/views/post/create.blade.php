47d761a4211290b68ac62d3f59c74f66efe9697b
JMarze <arze.jesus@gmail.com>
2016-10-07 18:28:13 -0400

    Vistas Parciales

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -11,58 +11,12 @@
                 </div>
 
                 <div class="panel-body">
-                    @if(count($errors) > 0)
-                    <div class="validation-messages">
-                        <ul>
-                            @foreach($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                    @endif
+
+                    @include('post.partial.validation')
 
                     {!! Form::open(['route' => 'admin.post.store', 'method' => 'POST', 'class' => 'form-horizontal']) !!}
 
-                    <div class="form-group{{ $errors->has('codigo')?' has-error':'' }}">
-                        {!! Form::label('codigo', 'Código', ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-6">
-                            {!! Form::text('codigo', null, ['placeholder' => 'Ej. XXX-123456', 'class' => 'form-control']) !!}
-
-                            @if($errors->has('codigo'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('codigo') }}</strong>
-                            </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        {!! Form::label('titulo', 'Título', ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-6">
-                            {!! Form::text('titulo', null, ['placeholder' => 'Ej. Nuevo post', 'class' => 'form-control']) !!}
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        {!! Form::label('contenido', 'Contenido', ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-6">
-                            {!! Form::textarea('contenido', null, ['placeholder' => 'Ej. Contenido del post', 'class' => 'form-control']) !!}
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        {!! Form::label('publicado', '¿Publicado?', ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-6">
-                            {!! Form::select('publicado', [true => 'Si', false => 'No'], true, ['class' => 'form-control']) !!}
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        {!! Form::label('categoria_id', 'Categoría', ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-6">
-                            {!! Form::select('categoria_id', $categorias, null, ['class' => 'form-control', 'placeholder' => 'Seleccione una categoría']) !!}
-                        </div>
-                    </div>
+                    @include('post.partial.form')
 
                     <div class="form-group">
                         <div class="col-md-6 col-md-offset-4">
---
+++ resources/views/post/partial/form.blade.php
@@ -0,0 +1,40 @@
+<div class="form-group{{ $errors->has('codigo')?' has-error':'' }}">
+    {!! Form::label('codigo', 'Código', ['class' => 'col-md-4 control-label']) !!}
+    <div class="col-md-6">
+        {!! Form::text('codigo', null, ['placeholder' => 'Ej. XXX-123456', 'class' => 'form-control']) !!}
+
+        @if($errors->has('codigo'))
+        <span class="help-block">
+            <strong>{{ $errors->first('codigo') }}</strong>
+        </span>
+        @endif
+    </div>
+</div>
+
+<div class="form-group">
+    {!! Form::label('titulo', 'Título', ['class' => 'col-md-4 control-label']) !!}
+    <div class="col-md-6">
+        {!! Form::text('titulo', null, ['placeholder' => 'Ej. Nuevo post', 'class' => 'form-control']) !!}
+    </div>
+</div>
+
+<div class="form-group">
+    {!! Form::label('contenido', 'Contenido', ['class' => 'col-md-4 control-label']) !!}
+    <div class="col-md-6">
+        {!! Form::textarea('contenido', null, ['placeholder' => 'Ej. Contenido del post', 'class' => 'form-control']) !!}
+    </div>
+</div>
+
+<div class="form-group">
+    {!! Form::label('publicado', '¿Publicado?', ['class' => 'col-md-4 control-label']) !!}
+    <div class="col-md-6">
+        {!! Form::select('publicado', [true => 'Si', false => 'No'], true, ['class' => 'form-control']) !!}
+    </div>
+</div>
+
+<div class="form-group">
+    {!! Form::label('categoria_id', 'Categoría', ['class' => 'col-md-4 control-label']) !!}
+    <div class="col-md-6">
+        {!! Form::select('categoria_id', $categorias, null, ['class' => 'form-control', 'placeholder' => 'Seleccione una categoría']) !!}
+    </div>
+</div>
